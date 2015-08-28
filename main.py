34fd61739355e1504c0c8c2bd65ece3e3f7bec67
DelbertCV <dfcustodiovargas@gmail.com>
2015-08-27 22:40:42 -0600

    Final

--- main.py
+++ main.py
@@ -8,51 +8,50 @@ def proceso(evento, name, t, cpu, nmt, nm, ni, espera):
     global TT
     global TL
     
-    yield evento.timeout(t)
-    nmt.get(nm)
+    yield evento.timeout(t) #Espera para crearse el proceso
+    nmt.get(nm) #Se toma la memoria necesaria de la ram
     t1 = evento.now
     print ('Se creo el %s en el tiempo %s'%(name,t1))
     with  cpu.request() as req:
         yield req
-        while (ni>0):
+        while (ni>0): #Si posee varias instrucciones
                 t2 = evento.now
                 print('%s se encuentra en el CPU en el tiempo %s'%(name,t2))
                 if (ni>=3):
-                    ni = ni - 3
+                    ni = ni - 3 #se restan instrucciones
                     yield evento.timeout(1)
-                    if (espera == 2):
+                    if (espera == 2): #por si debe esperar
                         yield evento.timeout(random.randint(1,10))
                 else:
                     print('%s esperando en el tiempo %s'%(name,evento.now))
                     ni = 0
+        #Calculo de tiempos promedio
         t2 = evento.now
         TP = t2 - t1
         TL.append(TP)
         TT = TT + TP
-        print('%s termina en el tiempo %s'%(name,t2))
-        T = TT/NP
-        print('El tiempo promedio  es de %7.4f'%(T))
-        nmt.put(nm)
+        nmt.put(nm) #Se devuelve la memoria al container
 
 
 global NP
-TT = 0
-TL = []
-NP = 25
-random.seed(50)
+TT = 0 #Tiempo total
+TL = [] #Lista para guardar los tiempos de cada proceso
+NP = 25 #Numero de procesos
+random.seed(50) #Seed utilizada
 evento = simpy.Environment()
-nmt = simpy.Container(evento,200, 100)
+nmt = simpy.resources.container.Container(evento,200, 100)
 cpu = simpy.Resource(evento,capacity = 1)
 for i in range(NP):
-    ins = random.randint(1,10)
-    nm = random.randint(1,10)
-    espera = random.randint(1,2)
-    t = random.expovariate(1.0/10)
+    ins = random.randint(1,10) #No de instrucciones
+    nm = random.randint(1,10) #No de memoria necesaria
+    espera = random.randint(1,2) #Debe esperar o no?
+    t = random.expovariate(1.0/10) #indicado en la guia
     evento.process(proceso(evento,'Proceso #%d'%i,t,cpu,nmt, nm,ins,espera))
     
-evento.run()
+evento.run() #Empieza simulacion
 TT = TT/NP
 
+#Calculo de desviacion estandar
 desviacion = 0
 for i in range(0,len(TL)):
     x = TL[i]
@@ -61,7 +60,7 @@ for i in range(0,len(TL)):
     desviacion = desviacion + x
     desviacion = desviacion / len(TL)
 desviacion = math.sqrt(desviacion)
-
+#Valores para comparar y realizar las graficas
 print('El tiempo promedio por proceso es de %7.4f y la desviacion estandar de %7.4f' % (TT,desviacion))
 
     
